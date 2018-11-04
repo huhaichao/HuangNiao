a384662ee4a0e2fed42d2401e465713385b3b7cf
chao.hu <chao.hu@okcoin.com>
2018-11-04 11:23:29 +0800

    自动清除日志脚本

--- bin/dellog.sh
+++ bin/dellog.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find /mnt/app/huangniao/huangniaoapp/logs -mtime +10  -name "*.out.*" -exec rm -rf {} \;
+find /mnt/app/huangniao/huangniaoapp/logs -mtime +15  -name "*.out.*" -exec rm -rf {} \;
